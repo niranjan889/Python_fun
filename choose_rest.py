a68c42f46ecf8709dccca8f9f8a58f12ce4cb8ab
niranjan889 <niranjan889@users.noreply.github.com>
2016-04-22 17:55:26 -0400

    Added files via upload
    This module is designed to solve a confusion we often have. Selecting a place to eat. 
    The input to the function get_rest() is a list of options to select among.
    The returned result will be a single randomly selected item.

---
+++ choose_rest.py
@@ -0,0 +1,26 @@
+'''
+Created on 2016-04-02
+@author: Niranjan
+'''
+import numpy as np
+import sys
+
+class choose_rest():
+    
+    def __init__(self):
+        self.choices=[] #Initialize an empty list
+    
+    def get_rest(self,choices):
+        if type(choices)==type(self.choices):   #check if the input type is list
+            self.choices=choices
+            choice=np.random.choice(self.choices)   #Select a restaurant randomly
+            return choice
+        else:
+            print 'Error! Incorrect input type: {}\nInput type must be a {}'.format(type(choices),type(self.choices))
+            sys.exit()
+
+if __name__=='__main__':
+    my_choices=['Italian','Indian','Chinese','Thai']
+    my_rest=choose_rest()
+    ch=my_rest.get_rest(my_choices)
+    print 'Congrats! You are going to have {} today.'.format(ch)
