10da7b342ef799b605cf0eebb742bbdafa306135
Niranjan Jadhav <niranjan889@users.noreply.github.com>
2016-07-20 23:40:04 -0400

    Add files via upload

---
+++ send_email.py
@@ -0,0 +1,25 @@
+import smtplib
+
+def Send_Email(msg):
+
+    FROMADDR = "xyz@abc.com"
+    # provide your login  
+    LOGIN    = "xyz@abc.com"
+    # provide your password 
+    PASSWORD = "" 
+    TOADDRS  = ["xyz@abc.com"] 
+    SUBJECT  = msg 
+    msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" 
+    % (FROMADDR, ", ".join(TOADDRS), SUBJECT) ) 
+    msg += msg+"\r\n"
+    server = smtplib.SMTP('smtp.gmail.com:587') 
+    server.set_debuglevel(1) 
+    server.ehlo() 
+    server.starttls() 
+    server.login(LOGIN, PASSWORD) 
+    server.sendmail(FROMADDR, TOADDRS, msg) 
+    server.quit()
+    
+if __name__ == '__main__':
+    
+    Send_Email('Program Terminated With Error code XXX')
