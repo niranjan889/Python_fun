5efbf264222db791dd6ba34acc9985754efccced
Niranjan Jadhav <niranjan889@users.noreply.github.com>
2016-07-27 22:21:02 -0400

    Update api_keys.py

--- foursquare/api_keys.py
+++ foursquare/api_keys.py
@@ -1,3 +1,5 @@
 ''' Keep the app secret and ID in this file'''
+# The client id and client secret can be found on your application's
+# Details page located at https://foursquare.com/oauth/
 FOURSQUARE_ID=''
 FOURSQUARE_SECRET=''
