e1bcc6def5ef681e86f5db62e4e12eaec80a1b2c
Niranjan Jadhav <niranjan889@users.noreply.github.com>
2016-09-20 20:14:02 -0400

    Update path_distance.py

--- path_distance.py
+++ path_distance.py
@@ -16,40 +16,34 @@ class path_distance():
         self.path = os.getcwd()
         
     def traverse(self):
+        #Open a file to write the outputs to
         with open(self.path+'/Path_output.csv','w') as fp2:
             writer=csv.writer(fp2)
+            #Open the file to read the data
             with open(self.path+'/Path distance.csv','r') as fp:
                 reader=csv.reader(fp)
-                next(reader, None)
-                cnt=0
+                next(reader, None)  #Skip the header
+                
+                #Traverse through each row
                 for row in reader:
-                    self.tot_dist = 0
+                    self.tot_dist = 0   #Initialize the distance to 0
+                    #Skipping the 1st column traverse the columns with a step of 2
                     for i in range(1, len(row)-2, 2):
-                        if row[i+2] in (None,""):
+                        if row[i+2] in (None,""):   #Check if there are any more columns to read
                             continue
                         else:
                             self.find_distance( (float(row[i]),float(row[i+1])), (float(row[i+2]),float(row[i+3])) )
-                    cnt+=1
                     print self.tot_dist
                     writer.writerow([self.tot_dist])
 
         fp2.close()
         fp.close()
-        
+    
+    #Function to calculate distance between 2 coordinates given as input
     def find_distance(self,p1,p2):
-        
         self.tot_dist += vincenty(p1,p2).miles
- 
-def google_distance():
-    orig_coord = -84.75950132, 43.60729635
-    dest_coord = -84.75946273, 43.60729638
-
-    print vincenty(orig_coord,dest_coord).miles
-    sys.exit()   
     
 if __name__== '__main__':
-#     google_distance()
     obj = path_distance()
     obj.path = os.getcwd()+'/data'
-    
     obj.traverse()
