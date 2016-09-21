0d1a956d84578633f66ab0cf8a567336529f7951
Niranjan Jadhav <niranjan889@users.noreply.github.com>
2016-09-20 20:29:21 -0400

    Update path_distance.py

--- path_distance.py
+++ path_distance.py
@@ -32,6 +32,7 @@ class path_distance():
                         if row[i+2] in (None,""):   #Check if there are any more columns to read
                             continue
                         else:
+                            #Pass the coordinates of 2 consecutive points in the sequence
                             self.find_distance( (float(row[i]),float(row[i+1])), (float(row[i+2]),float(row[i+3])) )
                     print self.tot_dist
                     writer.writerow([self.tot_dist])
